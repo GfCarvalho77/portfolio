ee7dfda91ff195bb727dc4228c197d7d3c86677e
GfCarvalho77 <127401543+GfCarvalho77@users.noreply.github.com>
2024-08-28 15:03:00 -0300

    Seção 'Sobre mim'

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://avatars.githubusercontent.com/u/127401543?v=4" alt="" />
+    <Titulo fontSize={20}>Guilherme Carvalho</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GitSecao } from './style'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, porro
+      voluptatem, autem placeat aliquam temporibus asperiores consequatur fugiat
+      architecto, perspiciatis minima illum quisquam culpa. Animi maiores porro
+      recusandae mollitia officia?
+    </Paragrafo>
+    <GitSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=GfCarvalho77&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=GfCarvalho77&layout=compact&langs_count=7&theme=dracula" />
+    </GitSecao>
+  </section>
+)
+
+export default Sobre
